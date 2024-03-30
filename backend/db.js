4b26bac17c4d0c54f226c3fc27421469515449f8
Mayank kumar <mayank30092003@gmail.com>
2024-03-30 14:17:58 +0530

    updating backend

--- backend/db.js
+++ backend/db.js
@@ -2,14 +2,33 @@ const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost:27017/paytm")
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
     username: String, 
     password: String,
     firstName: String,
     lastName: String,
 })
 
-const User = mongoose.model("User", userSchema);
+
+
+
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }, 
+    balance: {
+        type: Number,
+        required: true
+    }
+})
+
+const Account = mongoose.model("Account", accountSchema);
+const User = mongoose.model("User", userSchema);  
+
+
 module.exports = {
-    User
+    User,
+    Account
 }
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,9 @@
 const express = require("express")
 const zod = require("zod")
-const {User} = require("../db");
+const {User, Account} = require("../db");
 const jwt = require("jsonwebtoken");
 const {JWT_SECRET} = require("../config");
-
+const {authMiddleware} = require("../middleware");
 
 const router = express.Router();
 
@@ -44,6 +44,11 @@ router.post("/signUp", async(req, res) => {
 
     const userId = user._id;
 
+    await Account.create({
+        userId,
+        balance: 1+ Math.random()*10000
+        
+    })
     const token = jwt.sign({
         userId
     }, JWT_SECRET);
@@ -92,6 +97,86 @@ router.post("/signin", async(req, res) => {
     })
 
 
+})
+
+const updateBody = zod.object({
+    password: zod.string().optional(),
+    firstName: zod.string().optional(),
+    lastName: zod.string().optional(),
+})
+
+router.put("/", authMiddleware, async(req, res) => {
+    const {success} = updateBody.safeParse(req.body)
+    if(!success){
+        res.status(411).json({
+            message: "Error while updating information"
+        })
+    }
+
+    await User.updateOne(req.body, {
+        _id: req.userId
+    })
+
+    res.json({
+        message: "Updated successfully"
+    })
+})
+
+router.get("/bulk", async(req, res) => {
+    const filter = req.query.filter || "";
+    const users = await User.find({
+        $or: [{
+            firstName: {
+                "$regex": filter
+            }   
+        }, {
+            lastName: {
+                "$regex": filter
+            }
+        }]
+    })
+
+    res.json({
+        user: users.map(user => ({
+            username: user.username, 
+            firstName: user.firstName,
+            lastName: user.lastName, 
+            _id: user._id
+        }))
+    })
+})
+
+router.get("/balance", authMiddleware, async(req, res) => {
+    const account = await Account.findOne({
+        userId: req.userId
+    })
+
+    res.json({
+        balance: account.balance
+    })
+})
+
+router.post("/transfer", authMiddleware, async(req, res) => {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    const{amount, to} = req.body;
+
+    const account = await Account.findOne({userId: req.userId}).session(session);
+
+    if(!account || account.balance<amount){
+        await session.abortTransaction();
+        return res.status(400).json({
+            message: "Insufficient balance"
+        })
+    }
+
+    await Account.updateOne({userId: req.userId}, {$inc: {balance: -amount} }).session(session);
+    await Account.updateOne({userId: to}, {$inc: {balance: amount}}).session(session);
+
+    await session.commitTransaction();
+    res.json({
+        message: "Transfer successful"
+    })
 })
 
 module.exports = router;
